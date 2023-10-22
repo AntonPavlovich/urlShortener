fb74d133b9e3a585462c76a6a714141c2e87ea09
AntonPavlovich <antopavlu@gmail.com>
2023-10-22 14:40:35 +0300

    update build script

--- build.mjs
+++ build.mjs
@@ -1,13 +1,10 @@
 import * as esbuild from 'esbuild'
 
 await esbuild.build({
-  entryPoints: ['src/handlers/*', './src/enums', './src/utils/*'],
+  entryPoints: ['src/functions/**/*', './src/enums', './src/utils/*'],
   bundle: true,
   platform: 'node',
   target: ['node18'],
   outdir: './dist',
-  external: ['aws-sdk/clients/*', 'bcryptjs'],
-  loader: {
-    ".html": 'text'
-  }
+  external: ['aws-sdk/clients/*', 'bcryptjs', 'jsonwebtoken']
 })
