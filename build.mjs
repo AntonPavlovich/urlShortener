e2d870c7329f8707275e67d1a664728b40e26933
AntonPavlovich <antopavlu@gmail.com>
2023-10-23 12:20:17 +0300

    update build.mjs to read external dependencies from package.json

--- build.mjs
+++ build.mjs
@@ -1,10 +1,21 @@
-import * as esbuild from 'esbuild'
+import fs from 'fs';
+import path from "path";
+import * as esbuild from 'esbuild';
 
-await esbuild.build({
-  entryPoints: ['src/functions/**/*', './src/enums', './src/utils/*'],
-  bundle: true,
-  platform: 'node',
-  target: ['node18'],
-  outdir: './dist',
-  external: ['@aws-sdk/*', 'bcryptjs', 'jsonwebtoken']
+fs.readFile(path.resolve('./package.json'), { encoding: 'utf8' }, async (err, data) => {
+  const parsed = JSON.parse(data);
+  const external = Object.keys(parsed?.dependencies);
+
+  await esbuild.build({
+    entryPoints: ['src/functions/**/*', './src/enums', './src/utils/*'],
+    bundle: true,
+    platform: 'node',
+    target: ['node18'],
+    outdir: './dist',
+    external: [...external],
+  }).then(() => {
+    console.log('Builded!');
+  });
+
 })
+
