150e70f44f7e2614ef6ca1779c0df2f08e27d6c9
AntonPavlovich <antopavlu@gmail.com>
2023-10-25 16:48:11 +0300

    add ses client

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -3,6 +3,8 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 //sqs
 import { SQSClient } from "@aws-sdk/client-sqs";
+//ses
+import { SES } from '@aws-sdk/client-ses';
 
 export const getDynamoDbClient = (): DynamoDBClient => {
   return DynamoDBDocumentClient.from(new DynamoDBClient({}));
@@ -10,4 +12,8 @@ export const getDynamoDbClient = (): DynamoDBClient => {
 
 export const getSqsClient = (): SQSClient => {
   return new SQSClient({ region: process.env.REGION });
+}
+
+export const getSesClient = (): SES => {
+  return new SES({ region: process.env.REGION });
 }
