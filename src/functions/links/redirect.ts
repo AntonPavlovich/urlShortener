911065bbb9aee6a88b81679da03ced73eef0298a
AntonPavlovich <antopavlu@gmail.com>
2023-10-26 13:08:29 +0300

    fix redirect function to not store page in browser cache

--- src/functions/links/redirect.ts
+++ src/functions/links/redirect.ts
@@ -10,44 +10,52 @@ import { getDynamoDbClient } from '../../utils/shared';
 const ddb = getDynamoDbClient();
 
 export const redirect: Handler = async event => {
-  console.log(event);
   try {
     const shortId = String(event?.pathParameters?.shortId) ?? '';
 
-    const params: GetCommandInput = {
+    const getCommandParams: GetCommandInput = {
       TableName: process.env.LINKS_TABLE_NAME,
       Key: {
         ShortId: shortId
       },
     }
-    const { Item } = await ddb.send(new GetCommand(params));
+    const { Item } = await ddb.send(new GetCommand(getCommandParams));
     if (!Item || !Item.IsActive) {
       return {
-        statusCode: 400,
+        statusCode: 404,
         body: JSON.stringify({ message: 'Not found' })
       }
     }
-    if (Item.IsOneTime) {
-      const params: UpdateCommandInput = {
-        TableName: process.env.LINKS_TABLE_NAME,
-        Key: {
-          ShortId: shortId
-        },
-        UpdateExpression: 'SET IsActive = :active',
-        ExpressionAttributeValues: {
-          ":active": false,
-        },
+    const updateCommandParams: UpdateCommandInput = {
+      TableName: process.env.LINKS_TABLE_NAME,
+      Key: {
+        ShortId: shortId
+      },
+      UpdateExpression: 'SET Clicks = :clicks',
+      ExpressionAttributeValues: {
+        ':clicks': Item.Clicks + 1
       }
-      await ddb.send(new UpdateCommand(params))
     }
+    if (Item.IsOneTime) {
+      updateCommandParams.UpdateExpression += ' ,IsActive = :active';
+      updateCommandParams.ExpressionAttributeValues = {
+        ...updateCommandParams.ExpressionAttributeValues,
+        ':active': false
+      }
+    }
+    await ddb.send(new UpdateCommand(updateCommandParams));
 
     return {
       statusCode: 308,
       headers: {
-        Location: Item.OriginUrl,
+        'Location': Item.OriginUrl,
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        Pragma: 'no-cache',
+        Expires: 0,
       },
     }
   } catch (ex) {
+    console.error(ex);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Bad request' })
